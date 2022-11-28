eec443d748169c9709136e2c317ef1014cbfc0a0
josanaugustin <josanaugustin@gmail.com>
2022-11-28 12:31:12 +0530

    first commit

---
+++ src/main/java/com/example/demo/Login.java
@@ -0,0 +1,18 @@
+
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Login {
+
+	@Id
+	private String username;
+	private String password;
+	
+	
+}
